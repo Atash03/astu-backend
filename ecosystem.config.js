1127eee8f26eafef62317c872f240a9050d555ef
Atash03 <hugokarp12@gmail.com>
2026-03-10 18:20:28 +0500

    fix ecosystem conf

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,6 +6,7 @@ module.exports = {
       interpreter: "bun",
       args: "--env-file=.env.production",
       instances: 1,
+      exec_mode: 'fork',
       autorestart: true,
       watch: false,
       max_memory_restart: "512M",
