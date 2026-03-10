afb15af3eb11e84110805e6941f6ceb6b67f6572
Atash03 <hugokarp12@gmail.com>
2026-03-10 18:08:34 +0500

    add pm2 ecosystem config

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [
+    {
+      name: "astu-backend",
+      script: "src/web_server/srv.ts",
+      interpreter: "bun",
+      args: "--env-file=.env.production",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "512M",
+      env_production: {
+        NODE_ENV: "production",
+      },
+      env_development: {
+        NODE_ENV: "development",
+      },
+    },
+  ],
+};
