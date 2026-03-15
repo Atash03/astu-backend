600be00886798e4c2efb85b4b44d99d57245fac3
Atash03 <hugokarp12@gmail.com>
2026-03-16 00:17:45 +0500

    fix: ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,6 +12,8 @@ module.exports = {
       max_memory_restart: "512M",
       env_production: {
         NODE_ENV: "production",
+        PORT: 3030,
+        HOST: "127.0.0.1",
       },
       env_development: {
         NODE_ENV: "development",
